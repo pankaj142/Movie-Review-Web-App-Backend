1e5d975a19bb97f2a4672246b9fb1b2cf4888c32
pankaj142 <pankajagham1@gmail.com>
2021-01-18 01:26:03 +0530

    feat() : CRUD apis for movie review is achieved

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var body_parser = require("body-parser");
-var cors = require("cors");
 var path = require("path");
+const cors = require("cors");
 
 var app = express();
 const port = 3001;
 const reviews_routes = require("./routes/reviews");
 
 app.use(cors());
-app.use(body_parser.json())
+app.use(express.json());
+app.use(body_parser.urlencoded({extended:true}))
+// app.use(body_parser.json())
 
 app.use("/review", reviews_routes);
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,6 +5,7 @@ const db = require("../db/db");
 
 //get all Reviews items
 router.get('/', (req,res, next)=>{
+    console.log("get reviews")
     Reviews.find()
         .then(function(success){
             res.json({
@@ -31,6 +32,17 @@ router.get('/', (req,res, next)=>{
 
 //add new Reviews item
 router.post('/', (req,res, next)=>{
+    console.log("post review api", req.body)
+    if(req.body.movie_name == "" || req.body.review == ""){
+        res.json({
+            success : false,
+            error : {
+                "message" : "Compalsary fields are missing.",
+            },
+            data : {}
+        })
+        return false;
+    }
     let new_Reviews = new Reviews({
         movie_name : req.body.movie_name,
         review : req.body.review,
@@ -41,8 +53,6 @@ router.post('/', (req,res, next)=>{
                 success : false,
                 error : {
                     "message" : "Could not add the Reviews item.",
-                    "Reviews" : Reviews,
-                    "err" : err
                 },
                 data : {}
             })
@@ -60,8 +70,9 @@ router.post('/', (req,res, next)=>{
     })
 })
 
-router.delete('/:id', (req,res, next)=>{
-    Reviews.remove({_id : req.params.id}, (err, result)=>{
+router.delete('/:movie_name', (req,res, next)=>{
+    console.log("delete review api", req.params.movie_name)
+    Reviews.remove({movie_name : req.params.movie_name}, (err, result)=>{
         if(err){
             res.json({
                 success : false,
@@ -84,5 +95,42 @@ router.delete('/:id', (req,res, next)=>{
     })
 })
 
+//add new Reviews item
+router.post('/update', (req,res, next)=>{
+    console.log("post review api", req.body)
+    if(req.body.movie_name == "" || req.body.review == ""){
+        res.json({
+            success : false,
+            error : {
+                "message" : "Compalsary fields are missing.",
+            },
+            data : {}
+        })
+        return false;
+    }
+    let query = {movie_name : req.body.movie_name};
+    let update = {$set:{review:req.body.review}};
+    Reviews.findOneAndUpdate(query, update, (err)=>{
+        if(err){
+            res.json({
+                success : false,
+                error : {
+                    "message" : "Could not update the review",
+                },
+                data : {}
+            })
+            return false;
+        }else{
+            res.json({
+                success : true,
+                data : {
+                    "message" : "Reviews udpated successfully"
+                },
+                error : {}
+            })
+            return false;
+        }
+    })
+})
 
 module.exports = router;
